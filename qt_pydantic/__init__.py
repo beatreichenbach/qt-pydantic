38f32c87f37225959e62aa79099b4a85109a01ff
Beat Reichenbach <beatreichenbach@users.noreply.github.com>
2025-01-25 14:40:27 -0800

    feat: initial commit

---
+++ qt_pydantic/__init__.py
@@ -0,0 +1,16 @@
+from .annotations import (
+    QSize,
+    QSizeF,
+    QPoint,
+    QPointF,
+    QRect,
+    QRectF,
+    QDate,
+    QDateTime,
+    QTime,
+    QUuid,
+    QColor,
+    QFont,
+)
+
+__version__ = '0.0.0'
---
+++ tests/validation.py
@@ -0,0 +1,201 @@
+from typing import Any
+from pydantic import BaseModel, ValidationError
+import pytest
+from qtpy import QtCore, QtGui
+from qt_pydantic import (
+    QSize,
+    QSizeF,
+    QPoint,
+    QPointF,
+    QRect,
+    QRectF,
+    QDate,
+    QDateTime,
+    QTime,
+    QUuid,
+    QColor,
+    QFont,
+)
+
+
+# QSize
+@pytest.mark.parametrize(
+    'data,expected',
+    [
+        ((0, 1), QtCore.QSize(0, 1)),
+        ((-2, 3), QtCore.QSize(-2, 3)),
+        ([5, 0], QtCore.QSize(5, 0)),
+        ((0.8, 2.3), None),
+        ((1, 2, 3), None),
+    ],
+)
+def test_qsize(data: Any, expected: QtCore.QSize | None) -> None:
+
+    class Model(BaseModel):
+        data: QSize
+
+    if expected:
+        assert Model(data=data).data == expected
+    else:
+        with pytest.raises(ValidationError):
+            assert Model(data=data).data == expected
+
+
+# QSizeF
+@pytest.mark.parametrize(
+    'data,expected',
+    [
+        ((0.0, 1.2), QtCore.QSizeF(0, 1.2)),
+        ((-2.34, 3), QtCore.QSizeF(-2.34, 3)),
+        ([5.1, 0.1], QtCore.QSizeF(5.1, 0.1)),
+        ((1.1, 2.1, 3.1), None),
+    ],
+)
+def test_qsizef(data: Any, expected: QtCore.QSizeF | None) -> None:
+
+    class Model(BaseModel):
+        data: QSizeF
+
+    if expected:
+        assert Model(data=data).data == expected
+    else:
+        with pytest.raises(ValidationError):
+            assert Model(data=data).data == expected
+
+
+# QPoint
+@pytest.mark.parametrize(
+    'data,expected',
+    [
+        ((0, 1), QtCore.QPoint(0, 1)),
+        ((-2, 3), QtCore.QPoint(-2, 3)),
+        ([5, 0], QtCore.QPoint(5, 0)),
+        ((0.8, 2.3), None),
+        ((1, 2, 3), None),
+    ],
+)
+def test_qpoint(data: Any, expected: QtCore.QPoint | None) -> None:
+
+    class Model(BaseModel):
+        data: QPoint
+
+    if expected:
+        assert Model(data=data).data == expected
+    else:
+        with pytest.raises(ValidationError):
+            assert Model(data=data).data == expected
+
+
+# QPointF
+@pytest.mark.parametrize(
+    'data,expected',
+    [
+        ((0.0, 1.2), QtCore.QPointF(0, 1.2)),
+        ((-2.34, 3), QtCore.QPointF(-2.34, 3)),
+        ([5.1, 0.1], QtCore.QPointF(5.1, 0.1)),
+        ((1.1, 2.1, 3.1), None),
+    ],
+)
+def test_qpointf(data: Any, expected: QtCore.QPointF | None) -> None:
+
+    class Model(BaseModel):
+        data: QPointF
+
+    if expected:
+        assert Model(data=data).data == expected
+    else:
+        with pytest.raises(ValidationError):
+            assert Model(data=data).data == expected
+
+
+# QRect
+@pytest.mark.parametrize(
+    'data,expected',
+    [
+        ((0, 1, 2, 3), QtCore.QRect(0, 1, 2, 3)),
+        ((-2, 3, -1, -4), QtCore.QRect(-2, 3, -1, -4)),
+        ([5, 0, 2, 3], QtCore.QRect(5, 0, 2, 3)),
+        ((0.8, 2.3, 1.2, 3.4), None),
+        ((1, 2, 3, 4, 5), None),
+    ],
+)
+def test_qrect(data: Any, expected: QtCore.QRect | None) -> None:
+
+    class Model(BaseModel):
+        data: QRect
+
+    if expected:
+        assert Model(data=data).data == expected
+    else:
+        with pytest.raises(ValidationError):
+            assert Model(data=data).data == expected
+
+
+# QRectF
+@pytest.mark.parametrize(
+    'data,expected',
+    [
+        ((0.0, 1.2, 2.3, 3.4), QtCore.QRectF(0, 1.2, 2.3, 3.4)),
+        ((-2.34, 3, -1.2, -3.4), QtCore.QRectF(-2.34, 3, -1.2, -3.4)),
+        ([5.1, 0.1, 0.0, -1.2], QtCore.QRectF(5.1, 0.1, 0.0, -1.2)),
+        ((1.1, 2.1, 3.1, 4.1, 5.1), None),
+    ],
+)
+def test_qrectf(data: Any, expected: QtCore.QRectF | None) -> None:
+
+    class Model(BaseModel):
+        data: QRectF
+
+    if expected:
+        assert Model(data=data).data == expected
+    else:
+        with pytest.raises(ValidationError):
+            assert Model(data=data).data == expected
+
+
+# QDate
+@pytest.mark.parametrize(
+    'data,expected',
+    [
+        ('2000-01-05', QtCore.QDate(2000, 1, 5)),
+        (1679616000.0, QtCore.QDate(2023, 3, 24)),
+        ('2000-jan-05', None),
+        ('2000-1-5', None),
+    ],
+)
+def test_qdate(data: Any, expected: QtCore.QDate | None) -> None:
+
+    class Model(BaseModel):
+        data: QDate
+
+    if expected:
+        assert Model(data=data).data == expected
+    else:
+        with pytest.raises(ValidationError):
+            assert Model(data=data).data == expected
+
+
+# QDateTime
+@pytest.mark.parametrize(
+    'data,expected',
+    [
+        (
+            '2032-04-23T10:20:30.400+02:30',
+            QtCore.QDateTime(
+                QtCore.QDate(2032, 4, 23),
+                QtCore.QTime(10, 20, 30, 400),
+                QtCore.QTimeZone((2 * 3600) + (30 * 60)),
+            ),
+        ),
+    ],
+)
+def test_qdatetime(data: Any, expected: QtCore.QDateTime | None) -> None:
+
+    class Model(BaseModel):
+        data: QDateTime
+
+    if expected:
+        assert Model(data=data).data == expected
+    else:
+        with pytest.raises(ValidationError):
+            assert Model(data=data).data == expected
